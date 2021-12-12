38e5f34e2f2c7d0ac06848686d0c080d30426a46
Suzan-Hiary <suze-hiary@hotmail.com>
2021-12-12 22:30:57 +0200

    feat : complete the footer component

--- Search_topics/Search_topics.js
+++ Search_topics/Search_topics.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import 'tailwindcss/tailwind.css'
 
-
 function Search_topics() {
-  
+
     return (
         <div className="bg-slate-100 mt-0  bg-[url('https://media.istockphoto.com/photos/femalehand-of-planner-writing-daily-appointment-picture-id1173258771?k=20&m=1173258771&s=612x612&w=0&h=kXaXoxUwBESwIwta-RInWd2voCvLn0VjkJLggCP8hUQ=')] bg-cover ">
             <div className="w-80  mt-6   grid grid-cols-3 gap-96 border-x-0 " data-options="autoplay: 1000; step: 1; mode: carousel; height: 275;">
                 <div className="w-80  mt-6 mr-10 ml-10  grid grid-rows-6 grid-flow-col gap-4  ">
-                    <img alt="image" src="https://cdn.pixabay.com/photo/2021/12/07/14/00/river-6853339__480.jpg" className="w-full relative h-50 ease-in-out" />
-                    <img alt="image" src="https://cdn.pixabay.com/photo/2021/12/03/08/50/nature-6842159__340.jpg" className="w-full relative h-50 ease-in-out" />
-                    <img alt="image" src="https://cdn.pixabay.com/photo/2021/11/01/08/39/petit-minou-lighthouse-6759731__340.jpg" className="w-full relative h-50 ease-in-out " />
+                    <img alt="image" src="https://media.istockphoto.com/photos/goal-plan-action-text-on-note-pad-picture-id1284696245?b=1&k=20&m=1284696245&s=170667a&w=0&h=C5x9jqbc3oKHdmB52tBv-BKdYnLBaL-NH5gw9_pdSGc=" />
+                    <img alt="image" src="https://media.istockphoto.com/photos/flipping-of-wooden-cube-block-for-change-2021-to-2022-year-for-merry-picture-id1306669120?b=1&k=20&m=1306669120&s=170667a&w=0&h=OPcBy2EUkv2R5AV54XJqNniU2wNh2DI_dSZEcgDW6A0=" className="w-full relative h-50 ease-in-out" />
+                    <img alt="image" src="https://media.istockphoto.com/photos/asian-lady-typing-laptop-calendar-cafe-concept-picture-id598084402?b=1&k=20&m=598084402&s=170667a&w=0&h=ZH5ueGA9SOl-aVzUXiKKGQXbfzYLTcBEaZ31d_EsdEU=" className="w-full relative h-50 ease-in-out" className="w-full relative h-50 ease-in-out " />
 
                 </div>
                 <div className="bg-teal-400 w-full h-full float-right"
@@ -24,7 +23,7 @@ function Search_topics() {
                     </div>
                 </div>
 
-             
+
 
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from 'next/head'
 import Header from '../Search_topics/Header'
 import Search_topics from '../Search_topics/Search_topics'
+import Footer from '../Search_topics/Footer'
 
 export default function Home() {
   return (
     <div>
       <Header/>
       <Search_topics/>
+      <Footer/>
     
     </div>
   )
