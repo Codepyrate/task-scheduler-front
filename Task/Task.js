7c384bb78682854ecf852a2e9fac677f6d7ede95
KhaledAqrainy <khaledalqrainy@gmail.com>
2021-12-13 17:05:28 +0200

    add edit function and delete

--- Task/Task.js
+++ Task/Task.js
@@ -57,6 +57,67 @@ const App = () => {
     setEditFormData(newFormData);
   };
 
+  const handleAddFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTask = {
+      id: nanoid(),
+      TaskTitle: addFormData.TaskTitle,
+      Description: addFormData.Description,
+      TaskTime: addFormData.TaskTime,
+    };
+
+    const newTasks = [...task, newTask];
+    setTask(newTasks);
+  };
+
+  const handleEditFormSubmit = (event) => {
+    event.preventDefault();
+
+    const editedTask = {
+      id: editTaskId,
+      TaskTitle: editFormData.TaskTitle,
+      Description: editFormData.Description,
+      TaskTime: editFormData.TaskTime,
+    };
+
+    const newTasks = [...task];
+
+    const index = task.findIndex((task) => task.id === editTaskId);
+
+    newTasks[index] = editedTask;
+
+    setTask(newTasks);
+    setEditTaskId(null);
+  };
+
+  const handleEditClick = (event, task) => {
+    event.preventDefault();
+    setEditTaskId(task.id);
+
+    const formValues = {
+      TaskTitle: task.TaskTitle,
+      Description: task.Description,
+      TaskTime: task.TaskTime,
+    };
+
+    setEditFormData(formValues);
+  };
+
+  const handleCancelClick = () => {
+    setEditTaskId(null);
+  };
+
+  const handleDeleteClick = (taskId) => {
+    const newTasks = [...task];
+
+    const index = task.findIndex((task) => task.id === taskId);
+
+    newTasks.splice(index, 1);
+
+    setTask(newTasks);
+  };
+
 
 
   return (
